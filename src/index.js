e98458298444e3b3543409001054349687d91b85
Jean <zhan.yessim@gmail.com>
2022-02-23 14:30:12 +0600

    fix: complete towel sorting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
+module.exports = function towelSort(matrix) {
+if(matrix == undefined) {
   return [];
+} else {
+    return matrix.reduce((acc, current, index) => {
+    return acc.concat(((index % 2 == 0) ? current : current.reverse()));
+  }, [])
 }
+};
